9252cbb30f353ef9334faac0eb16f2219f2c822a
Alexis Lopez Zubieta <contact@azubieta.net>
2019-10-08 19:10:36 -0500

    initialize git submodules

--- install-linuxdeploy.sh
+++ install-linuxdeploy.sh
@@ -6,6 +6,7 @@ source /entrypoint.sh
 
 git clone https://github.com/linuxdeploy/linuxdeploy.git --depth=1 /tmp/linuxdeploy
 pushd /tmp/linuxdeploy
+git submodule update --init --recursive
 cmake . -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release
 make -j`nproc`
 make install
